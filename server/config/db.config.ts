2e2e663887ca00c17d0c9ea43a3ee390c386794c
Sheikh Danish Iqbal <danishiqbal4@gmail.com>
2023-04-16 13:26:29 +0400

    Add DB config

---
+++ server/config/db.config.ts
@@ -0,0 +1,19 @@
+import { DbConfig } from "../types/types.db";
+
+const DbConfig: DbConfig = {
+    HOST: "ep-snowy-recipe-577928.ap-southeast-1.aws.neon.tech",
+    USER: "danishiqbal4",
+    PASSWORD: "dBEeNpf6bIZ4",
+    DB: "jam",
+    dialect: "postgres",
+    // operatorsAliases: false,
+    sslmode: "require",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+export default DbConfig;
